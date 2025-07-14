63ba523018e49e193305075ee7c5c4885bbdae43
annaKosonog <anna.kosonog21@gmail.com>
2025-07-14 18:24:26 +0200

    Pierwszy commit: dodano strukturę projektu Java

---
+++ src/main/java/org/nauka/management_system_for_university/ManagementSystemForUniversityApplication.java
@@ -0,0 +1,13 @@
+package org.nauka.management_system_for_university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemForUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementSystemForUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nauka/management_system_for_university/ManagementSystemForUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nauka.management_system_for_university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemForUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
