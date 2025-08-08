7207204b6d4a8224a7b9b70f58c4ab0e562c021a
annaKosonog <anna.kosonog21@gmail.com>
2025-08-08 21:37:30 +0200

    test: add unit tests for finding semester by ID

---
+++ src/test/java/org/nauka/service/SemesterServiceTest.java
@@ -0,0 +1,49 @@
+package org.nauka.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.nauka.model.SemesterTest;
+import org.nauka.model.dao.Semester;
+import org.nauka.repository.SemesterRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class SemesterServiceTest {
+    @Mock
+    SemesterRepository semesterRepository;
+
+    @InjectMocks
+    SemesterService semesterService;
+
+    @Test
+    void shouldReturnSemesterWhenSemesterExists() {
+        Long idSemester = 1L;
+        when(semesterRepository.findIdBySemester(idSemester)).thenReturn(SemesterTest.semesterSummer);
+
+        Semester result = semesterService.getSemesterById(idSemester);
+
+        assertNotNull(result);
+        assertEquals(idSemester, result.getSemesterId());
+
+        verify(semesterRepository).findIdBySemester(idSemester);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSemesterDoesNotExist() {
+        Long id = 99L;
+        when(semesterRepository.findIdBySemester(id)).thenReturn(null);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> semesterService.getSemesterById(id)
+        );
+        assertTrue(exception.getMessage().contains("Not found semester by id: " + id));
+        verify(semesterRepository).findIdBySemester(id);
+    }
+}
